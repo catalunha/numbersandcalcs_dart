413fd714d676a80cc1bd49efbac89e844c2786b5
catalunha <catalunha.mj@gmail.com>
2023-03-28 04:46:14 -0300

    gcf e lcm

--- lib/numbersandcalcs_dart.dart
+++ lib/numbersandcalcs_dart.dart
@@ -1,16 +1,10 @@
+import 'alg.dart';
+
 void calculate() {
-  List<int> list1 = [1, 2];
-  // List<int> list1 = [1, 2, 3];
-  String operation = '+';
-  List<int> list2 = [1, 2, 3];
-  // List<int> list2 = [1, 2];
-  List<int> ans = [];
-  int amount = 1;
-  for (var n1 in list1) {
-    for (var n2 in list2) {
-      // print('$n1 $operation $n2');
-      int ans = n1 + n2;
-      print('$n1 $operation $n2 = $ans');
-    }
-  }
+  int num1 = 4;
+  int num2 = 8;
+  print('GCF = ${algGCF(num1, num2)}');
+  print('GCF = ${algGCF(num2, num1)}');
+  print('LCM = ${algLCM(num1, num2)}');
+  print('LCM = ${algLCM(num2, num1)}');
 }
