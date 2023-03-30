98019d076255bc6943e63285e96ee80e8d328622
catalunha <catalunha.mj@gmail.com>
2023-03-30 05:43:22 -0300

    revisao mynumber

--- bin/main.dart
+++ bin/main.dart
@@ -5,11 +5,10 @@ import 'package:numbersandcalcs_dart/my_number/example_pot.dart';
 import 'package:numbersandcalcs_dart/my_number/example_sub.dart';
 import 'package:numbersandcalcs_dart/my_number/example_sum.dart';
 import 'package:numbersandcalcs_dart/my_number/example_sqrt.dart';
-import 'package:numbersandcalcs_dart/my_number/isolate.dart';
 
 void main(List<String> arguments) {
   // exampleGCFandLCM();
-  isolateTest();
+  // isolateTest();
   // exit(0);
   exampleSum();
   exampleSub();
